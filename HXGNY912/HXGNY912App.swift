b61b87021d71308d32a11b59dd6b6fb28747bda6
Feng Chen <feng.chen100@gmail.com>
2026-09-12 23:02:48 -0400

    Initial Commit

---
+++ HXGNY912/HXGNY912App.swift
@@ -0,0 +1,17 @@
+//
+//  HXGNY912App.swift
+//  HXGNY912
+//
+//  Created by Amy Chen on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HXGNY912App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
